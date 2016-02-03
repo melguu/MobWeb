c053e99169decd104434118897b2fe4473dd0e4d
Arto Heino <Artsi@Arto-MacBook-Pro.local>
2016-02-03 13:19:11 +0200

    New directory tree

--- src/ajaxController.js
+++
@@ -1,6 +0,0 @@
-angular.module('kuveij')
-    .controller('ajaxController', function ($scope, ajaxService) {
-        ajaxService.success(function (data) {
-            $scope.files = data;
-        });
-    });
