07e5459b5331667e5d86407214a12486f9a24a97
Carla Miettinen <carla.lausamo@nuva.fi>
2016-02-11 12:44:03 +0200

    etusivu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,17 +2,22 @@
  * Created by Artsi on 28/01/16.
  */
 angular.module('kuveij', ['ngRoute'])
-    .config(function($routeProvider) {
-    $routeProvider
+    .config(function ($routeProvider) {
+        $routeProvider
         // route for the home page
-        .when('/', {
-            templateUrl : 'views/allMedia.html',
-            controller  : 'mediaController'
+            .when('/', {
+            templateUrl: 'views/allMedia.html',
+            controller: 'mediaController'
         })
 
         // route for the about page
         .when('/myImages', {
-            templateUrl : 'views/myImages.html',
-            controller  : 'uploadController'
+            templateUrl: 'views/myImages.html',
+            controller: 'uploadController'
+        })
+
+        .when('/image/:id', {
+            templateUrl: 'views/oneImage.html',
+            controller: 'singleMediaController'
         });
-});
+    });
