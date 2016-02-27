21f1882497b5b450402592cc7a4377c76c5f3ff7
Arto Heino <Artsi@Arto-MacBook-Pro.local>
2016-02-27 17:08:52 +0200

    Pagination redone, sort redone

--- src/scripts/controllers/mediaController.js
+++ src/scripts/controllers/mediaController.js
@@ -1,49 +1,24 @@
 angular.module('kuveij')
     .controller('mediaController', ['$scope', '$sce', 'ajaxFactory', 'MediaService',
         function ($scope, $sce, ajaxFactory, MediaService) {
-            $scope.fileType = undefined;
+            $scope.perPage = 12;
 
             $scope.changeFileType = function(type){
-                $scope.fileType = type;
+                if( typeof type === "undefined" ){
+                    ajaxFactory.loadAllMedia().success(function (response) {
+                        $scope.files = response;
+                    });
+                } else {
+                    ajaxFactory.loadSpecifiedMedia(type).success(function (response){
+                        $scope.files = response;
+                    });
+                }
             };
-            console.log($scope.fileType);
 
-            if($scope.fileType === undefined){
-                ajaxFactory.loadAllMedia().success(function (response) {
-                    loadItemsOnPage(response);
-                });
-            }else{
-                ajaxFactory.loadSpecifiedMedia(fileType).success(function (response){
-                    loadItemsOnPage(response);
-                });
-            }
-
-            function loadItemsOnPage(fileList){
-                $scope.files = fileList;
-                $scope.itemsPerPage = 12;
-                $scope.currentPage = 0;
-                $scope.total = $scope.files.length;
-                $scope.pagedFiles = $scope.files.slice($scope.currentPage * $scope.itemsPerPage,
-                    $scope.currentPage * $scope.itemsPerPage + $scope.itemsPerPage);
-
-                $scope.loadMore = function () {
-                    $scope.currentPage++;
-                    var newItems = $scope.files.slice($scope.currentPage * $scope.itemsPerPage,
-                        $scope.currentPage * $scope.itemsPerPage + $scope.itemsPerPage);
-                    $scope.pagedFiles = $scope.pagedFiles.concat(newItems);
-                };
-
-                $scope.nextPageDisabledClass = function () {
-                    return $scope.currentPage === $scope.pageCount() - 1 ? "disabled" : "";
-                };
-
-                $scope.pageCount = function () {
-                    return Math.ceil($scope.total / $scope.itemsPerPage);
-                };
-            }
 
             $scope.trustSrc = function (src) {
                 return $sce.trustAsResourceUrl(MediaService.mediaUrl + src);
             };
 
+            $scope.changeFileType();
         }]);
